ec86bb9c8bac6d563bd74051f6141df6d92d244b
jpreyes <juanpabloreyesg@gmail.com>
2014-08-29 14:07:19 -0500

    last part

--- Documents/rails_projects/simulador/app/models/credit.rb
+++ Documents/rails_projects/simulador/app/models/credit.rb
@@ -8,9 +8,110 @@ class Credit < ActiveRecord::Base
 	validates :lineaCredito, presence: true
 
 
-def count
-	p "Total of credits" + Credit.count
-end
+	def self.generarPlanes
+		#Credit.all.each do |n|
+		#	if n.cedula == "1019040272"
+		#		n.update_attribute(:estado, "Procesado")
+		#	end
+		#end"
+		#Thread.abort_on_exception=true
+		
+		threads = []
+		Credit.where(estado: "En proceso").each do |credit| 		
+			procesarCredito(credit)
+			calcularRiesgo
+			credit.estado = ("Generado")
+			credit.nivelRiesgo = rand(10) + 1
+			credit.save
+
+		end
+
+	end	
+
+			
+		
+		
+
+	def self.calcularRiesgo()
+		timeStart = Time.now
+		timeEnd = timeStart
+
+		num = 1
+		while timeEnd - timeStart <= 25 do
+			timeEnd = Time.now
+			
+			i = 2
+			while i <= num do
+				if num % i == 0
+					break
+				end
+				i += 1
+			end
+
+			num =+ 1
+
+		end
+
+
+	
+	end
+
+
+	def self.procesarCredito(credit)
+
+		i = 0
+			fee = nil
+
+			monthRate = credit.user.credit_lines.find(credit.lineaCredito).annualRate / 1200
+			feeNumber = (credit.plazo).to_d
+			creditValue = (credit.valorCredito).to_d
+			saldoPendiente = creditValue
+			
+			feeAmount = (creditValue * monthRate * (1 + monthRate ) ** feeNumber) / 
+													(((1 + monthRate ) ** feeNumber)  - 1)
+	
+
+				until i >= credit.plazo do
+			
+
+					fee = credit.fees.build
+
+							
+					fee.valor_cuota =	(feeAmount.round(2)).to_s		
+					fee.numero_cuota = i + 1
+					fee.amortizacion = ((feeAmount - creditValue*monthRate)*(1 + monthRate)**i).round(2).to_s
+					fee.pago_intereses = (feeAmount - fee.amortizacion.to_d).round(2).to_s
+					
+					saldoPendiente = saldoPendiente - fee.amortizacion.to_d
+					fee.saldo_pendiente = saldoPendiente.to_s
+					#fee.saldo_pendiente = (creditValue - feeAmount.to_i*(i+1)).to_s		
+
+					fee.save
+									#fee.update_attribute( :valor_couta, feeAmount.to_s)
+
+					i += 1
+				end
+
+	
+	end
+
+
+	def self.clean
+		Credit.all.each do |credit|
+			credit.estado = "En proceso"
+			credit.nivelRiesgo = "En proceso"
+			credit.save
+			credit.fees.all.each do |n|
+				n.delete
+			end
+		end
+
+	end
+
+	
+	
 
 end
+
+
 
